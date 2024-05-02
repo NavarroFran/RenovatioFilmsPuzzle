0ff26ae2d948020aec36a458f82723a7c5dc31bb
NavarroFran <navarro.franjav@gmail.com>
2024-05-02 18:58:07 -0300

    prueba host

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function cambiarColor(){
+    document.getElementById("Hola").style.color = "blue";
+}
+
+$(document).ready(function (){
+    document.getElementById("Hola").style.color = "blue";
+})
